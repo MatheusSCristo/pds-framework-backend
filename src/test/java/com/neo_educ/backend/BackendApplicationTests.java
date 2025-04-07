6a241b304ca825d7abcaf0902787a3bd9573d1ba
Matheus Cristo <cristo.matheus@outlook.com>
2025-04-07 20:53:12 -0300

    first commit

---
+++ src/test/java/com/neo_educ/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neo_educ.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
