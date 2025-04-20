e4ecd4e97c97b034483795f96ad1bd7c63230bd3
Matheus Raphael <matheusraphael0902@gmail.com>
2025-04-19 22:19:59 -0300

    feat: removed unused imports

--- src/main/java/com/neo_educ/backend/modules/notes/useCase/GetOneNoteInformationUseCase.java
+++ src/main/java/com/neo_educ/backend/modules/notes/useCase/GetOneNoteInformationUseCase.java
@@ -7,8 +7,6 @@ import com.neo_educ.backend.modules.notes.repository.NotesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class GetOneNoteInformationUseCase {
 
