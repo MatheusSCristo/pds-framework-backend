46e62b77c475d65ba56166e9941e11ddff2a3ee3
Raylanna <raylanna.l@hotmail.com>
2025-05-15 11:35:38 -0300

    feat: :sparkles: Add AI context

--- src/main/java/com/neo_educ/backend/modules/classplans/service/ClassPlansService.java
+++ src/main/java/com/neo_educ/backend/modules/classplans/service/ClassPlansService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.neo_educ.backend.exceptions.common.NotFoundException;
+import com.neo_educ.backend.modules.chat.service.ChatService;
 import com.neo_educ.backend.modules.classplans.dto.ClassPlansCreateDTO;
 import com.neo_educ.backend.modules.classplans.dto.ClassPlansUpdateDTO;
 import com.neo_educ.backend.modules.classplans.entity.ClassPlansEntity;
@@ -18,8 +19,22 @@ import com.neo_educ.backend.modules.teacher.repository.TeacherRepository;
 @Service
 public class ClassPlansService {
   
+  private final String iaClassPlansContext = "Você é um assistente didático para professores de inglês. Quando o professor digitar um assunto (por exemplo: Present Perfect ou Vocabulary about Travel), você deve gerar um plano de aula completo para uma aula de inglês. O plano deve ser objetivo, prático e adaptado a estudantes de inglês como segunda língua. O professor pode usar esse plano diretamente em sala de aula.\n" + //
+        "\n" + //
+        "O plano deve conter:\n" + //
+        "- Objetivo(s) da aula\n" + //
+        "- Materiais necessários (se houver)\n" + //
+        "- Duração estimada\n" + //
+        "- Etapas da aula (com sugestões de atividades para introdução, prática e encerramento)\n" + //
+        "- Dicas para o professor\n" + //
+        "- Sugestões de dever de casa (opcional)\n" + 
+        "Esse é o assunto:";
+
   @Autowired
   private ClassPlansRepository classPlansRepository;
+
+  @Autowired
+  private ChatService chatService;
 
   @Autowired
   private TeacherRepository teacherRepository;
@@ -30,12 +45,14 @@ public class ClassPlansService {
       
     ClassPlansEntity entity = new ClassPlansEntity();
 
+    String geminiResponse = this.chatService.chat(this.iaClassPlansContext + data.inputData());
+
     entity.setTopic(data.topic());
     entity.setClassDate(data.classDate());
     entity.setInputData(data.inputData());
     entity.setTeacher(teacher);
     entity.setStatus(ClassPlanStatus.PENDING);
-
+    entity.setAiGeneratedContent(geminiResponse);
 
     return classPlansRepository.save(entity);
 
