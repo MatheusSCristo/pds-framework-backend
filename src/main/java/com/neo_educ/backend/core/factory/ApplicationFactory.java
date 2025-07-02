2eb18fe837deffccf9c707498caa8d284823268c
Matheus Raphael <matheusraphael0902@gmail.com>
2025-07-02 11:35:11 -0300

    feat: Created methods in ApplicationFactory and refactored ActivityGeneratorService

--- src/main/java/com/neo_educ/backend/core/factory/ApplicationFactory.java
+++ src/main/java/com/neo_educ/backend/core/factory/ApplicationFactory.java
@@ -1,4 +1,12 @@
 package com.neo_educ.backend.core.factory;
 
+import com.neo_educ.backend.core.service.ActivityGeneratorService;
+import com.neo_educ.backend.modules.auth.service.AuthService;
+
 public interface ApplicationFactory {
+
+    AuthService createAuthService();
+    UserService createUserService();
+    ActivityGeneratorService createActivityGeneratorService();
+
 }
--- src/main/java/com/neo_educ/backend/core/service/ActivityGeneratorService.java
+++ src/main/java/com/neo_educ/backend/core/service/ActivityGeneratorService.java
@@ -1,7 +1,11 @@
 package com.neo_educ.backend.core.service;
 
+import com.neo_educ.backend.modules.materialGeneration.dto.GenerateExerciseDTO;
+
+import java.util.Map;
+
 public interface ActivityGeneratorService {
 
-    Object generateActivity();
+    String generateActivity(Map<String, Object> infos);
 
 }
