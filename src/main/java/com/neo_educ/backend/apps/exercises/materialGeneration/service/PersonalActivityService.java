bf00d909687ea23e80a05665a0ebde4b8e6a73c4
Matheus Raphael <matheusraphael0902@gmail.com>
2025-07-18 14:58:16 -0300

    refactor: Refact PersonalActivityService

--- src/main/java/com/neo_educ/backend/apps/exercises/materialGeneration/service/PersonalActivityService.java
+++ src/main/java/com/neo_educ/backend/apps/exercises/materialGeneration/service/PersonalActivityService.java
@@ -7,87 +7,42 @@ import com.neo_educ.backend.apps.exercises.materialGeneration.dto.GenerateAthlet
 import com.neo_educ.backend.apps.exercises.materialGeneration.dto.GenerateExerciseDTO;
 import com.neo_educ.backend.apps.exercises.materialGeneration.dto.GenerateMaterialDTO;
 import com.neo_educ.backend.apps.exercises.materialGeneration.utils.PersonalSentencesPromptTemplate;
-import com.neo_educ.backend.core.llm.service.LLMService;
 import com.neo_educ.backend.core.service.ActivityGeneratorService;
-import com.neo_educ.backend.exceptions.generateMaterial.ActivityGenerateException;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("exercisesActivityService")
-@AllArgsConstructor
-public class PersonalActivityService implements ActivityGeneratorService {
+public class PersonalActivityService extends ActivityGeneratorService<GenerateAthleteActivityDTO, GenerateAthleteReportDTO, GenerateMaterialDTO, GenerateExerciseDTO> {
 
-    private LLMService llmService;
+    @Autowired
     private PersonalSentencesPromptTemplate promptTemplate;
+    @Autowired
     private AthleteService athleteService;
 
+
     @Override
-    public String generateSession(String topic) {
-        try {
-            String prompt = promptTemplate.createWorkoutprompt(topic);
-            return llmService.chat(prompt);
-        } catch (Exception e) {
-            throw new ActivityGenerateException();
-        }
+    protected String buildPromptForSession(String topic) {
+        return promptTemplate.createWorkoutprompt(topic);
     }
 
     @Override
-    public String generateMaterialContent(Object dto) {
-        try {
-            if (dto instanceof GenerateMaterialDTO materialDTO) {
-                String prompt = promptTemplate.createMaterialPrompt(materialDTO);
-                return llmService.chat(prompt);
-            } else {
-                throw new IllegalArgumentException("Tipo de DTO não suportado para generateMaterialContent no PersonalActivityService.");
-            }
-        } catch (Exception e) {
-            throw new ActivityGenerateException();
-        }
+    protected String buildPromptForActivity(GenerateAthleteActivityDTO activityDto) {
+        AthleteResponseDTO athlete = athleteService.findById(activityDto.athleteId());
+        return promptTemplate.createActivityPrompt(activityDto, athlete);
     }
 
     @Override
-    public String generateActivityContent(Object data) {
-        try {
-            if(data instanceof GenerateAthleteActivityDTO activityDTO) {
-                AthleteResponseDTO athlete = athleteService.findById(activityDTO.athleteId());
-                String prompt = promptTemplate.createActivityPrompt(activityDTO,athlete);
-                return llmService.chat(prompt);
-            }
-            else{
-                return  null;
-            }
-        } catch (Exception e) {
-            throw new ActivityGenerateException();
-        }
+    protected String buildPromptForReport(GenerateAthleteReportDTO reportDto) {
+        return promptTemplate.createReportPrompt(reportDto.data());
     }
 
     @Override
-    public String generateReportContent(Object reportData) {
-        if(reportData instanceof GenerateAthleteReportDTO) {
-            try {
-                String prompt = promptTemplate.createReportPrompt(((GenerateAthleteReportDTO) reportData).data());
-                return llmService.chat(prompt);
-            } catch (Exception e) {
-            throw new ActivityGenerateException();
-            }
-        }
-        else{
-            throw new ClassCastException();
-        }
-
+    protected String buildPromptForExercise(GenerateExerciseDTO exerciseDto) {
+        return promptTemplate.createExercisePrompt(exerciseDto);
     }
 
     @Override
-    public String generateExerciseContent(Object dto) {
-        try {
-            if (dto instanceof GenerateExerciseDTO exerciseDTO) {
-                String prompt = promptTemplate.createExercisePrompt(exerciseDTO);
-                return llmService.chat(prompt);
-            } else {
-                throw new IllegalArgumentException("Tipo de DTO não suportado para generateStructuredTasks no PersonalActivityService.");
-            }
-        } catch (Exception e) {
-            throw new ActivityGenerateException();
-        }
+    protected String buildPromptForMaterial(GenerateMaterialDTO materialDto) {
+        return promptTemplate.createMaterialPrompt(materialDto);
     }
 }
